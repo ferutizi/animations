b99022262eeb0a25a87a31b396e0f20ba82ed534
ferutizi <fer.utizi@gmail.com>
2024-07-31 18:57:05 -0300

    add: variants animation scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,30 +17,52 @@ export default function Home() {
     setCurrentImage(next)
   }
 
+  const variants = {
+    initial: {
+      x: 200,
+      opacity: 0
+    },
+    animate: {
+      x: 0,
+      opacity: 1
+    },
+    exit: {
+      x: -200,
+      opacity: 0
+    },
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <section className="flex relative">
+      <section className="flex-col relative">
+        <motion.img
+          className="object-cover w-[600px] h-[300px] rounded-xl"
+          key={images[currentImage]}
+          variants={variants}
+          animate="animate"
+          initial="initial"
+          exit="exit"
+          src={`/${images[currentImage]}`}
+          alt="cat image"
+          title="cat"
+        /> 
         <button 
           onClick={() => prevImage()}
           className="flex justify-center items-center w-7 h-12 px-2 absolute m-auto top-0 bottom-0 left-4 bg-black rounded-xl bg-opacity-70 hover:bg-opacity-100 text-4xl"
         >
           <motion.span whileTap={{x: -6}}>{"◂"}</motion.span>
         </button>
-
-        <motion.img
-          className="object-cover w-[600px] h-[300px]"
-          src={`/${images[currentImage]}`}
-          alt="cat image"
-          title="cat"
-        > 
-        </motion.img>
-
         <button
           onClick={() => nextImage()}
           className="flex justify-center items-center w-7 h-12 px-2 absolute m-auto top-0 bottom-0 right-4 bg-black rounded-xl bg-opacity-70 hover:bg-opacity-100 text-4xl"
         >
           <motion.span whileTap={{x: 6}}>{"▸"}</motion.span>
         </button>
+        <div className="flex items-center justify-center gap-4 text-3xl">
+          <span className={`${currentImage !== 0 && "opacity-60"} cursor-pointer`} onClick={() => setCurrentImage(0)}>•</span>
+          <span className={`${currentImage !== 1 && "opacity-60"} cursor-pointer`} onClick={() => setCurrentImage(1)}>•</span>
+          <span className={`${currentImage !== 2 && "opacity-60"} cursor-pointer`} onClick={() => setCurrentImage(2)}>•</span>
+        </div>
       </section>
     </main>
   );
